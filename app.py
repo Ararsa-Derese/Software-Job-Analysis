60c83f39b69520b7b82526fac326ac48daede675
ararsa <ararsaderese6@gmail.com>
2024-06-09 01:11:29 +0300

    :art: finished all

--- app.py
+++ app.py
@@ -1,33 +1,106 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, request, render_template, jsonify
 import joblib
 import pandas as pd
+import numpy as np
+import pickle
+import io
+from io import BytesIO
+import base64
+from matplotlib.figure import Figure
+import matplotlib.pyplot as plt
 
 app = Flask(__name__)
 
-# Load the saved model
-model = joblib.load('salary_predictor_model.pkl')
+# Load the trained model and encoders/scalers
+model = joblib.load('pickels/linear_regression_model.pkl')
+degree_encode = joblib.load('pickels/degree_encoder.pkl')
+job_title_encoder = joblib.load('pickels/job_title_encoder.pkl')
+scaler_age = joblib.load('pickels/age_scaler.pkl')
+scaler_experience = joblib.load('pickels/experience_scaler.pkl')
+country_encoder = joblib.load('pickels/Country_encoder.pkl') 
+with open('pickels/arima_model.pkl', 'rb') as pkl_file:
+    arima_model_fit = pickle.load(pkl_file)
 
 @app.route('/')
 def home():
     return render_template('index.html')
+# Load and preprocess the trend prediction dataset
+trend_data = pd.read_csv('postings2.csv')
+trend_data['date_posted'] = pd.to_datetime(trend_data['date_posted'])
+trend_data.set_index('date_posted', inplace=True)
+monthly_job_postings = trend_data.resample('ME').size()
+
+# Load the saved ARIMA model
+with open('arima_model.pkl', 'rb') as pkl_file:
+    arima_model_fit = pickle.load(pkl_file)
+
 @app.route('/predict', methods=['POST'])
 def predict():
-    data = request.get_json()
-    age = data['Age']
-    gender = data['Gender']
-    education_level = data['Education Level']
-    job_title = data['Job Title']
-    years_of_experience = data['Years of Experience']
-    country = data['Country']
-    race = data['Race']
-    senior = data['Senior']
+    # year = int(request.form['year'])
+    start_year = request.form['start_year']
+    end_year = request.form['end_year']   
     
-    input_data = pd.DataFrame([[age, gender, education_level, job_title, years_of_experience, country, race, senior]],
-                              columns=['Age', 'Gender', 'Education Level', 'Job Title', 'Years of Experience', 'Country', 'Race', 'Senior'])
+    # Generate predictions for the entire year
+    date_range = pd.date_range(start=start_year, end=end_year, freq='ME')
     
-    prediction = model.predict(input_data)[0]
-    # print(prediction
-    return jsonify({'predicted_salary': prediction + ' per Year(Annualy)'})
+    # Calculate the number of steps to forecast
+    steps = len(date_range)
+    
+    # Generate forecasts
+    forecast = arima_model_fit.forecast(steps=steps)
+    forecast_index = pd.date_range(start=monthly_job_postings.index[-1], periods=steps+1, freq='ME')[1:]
+    forecast_series = pd.Series(forecast, index=forecast_index)
+    
+    # Plot the historical data and the forecast
+    plt.figure(figsize=(12, 6))
+    plt.plot(monthly_job_postings, label='Historical Data')
+    plt.plot(forecast_series, label='Forecast', color='red')
+    plt.axvline(x=monthly_job_postings.index[-1], linestyle='--', color='gray')
+    plt.title('Job Postings Forecast')
+    plt.xlabel('Date')
+    plt.ylabel('Number of Job Postings')
+    plt.legend()
+    plt.grid(True)
+    
+    # Save the plot as a PNG image in memory
+    img = io.BytesIO()
+    plt.savefig(img, format='png')
+    img.seek(0)
+    plot_url = base64.b64encode(img.getvalue()).decode('utf8')
+    
+    return jsonify({'plot_url': plot_url})
 
-if __name__ == '__main__':
+@app.route('/predict_salary', methods=['POST'])
+def predict_salary():
+    age = int(request.form['age'])
+    experience_years = int(request.form['experience_years'])
+    degree = request.form['degree']
+    job_title = request.form['job_title']
+    country = request.form['country']
+    
+    print(age, experience_years, degree, job_title)
+    # Ensure the input data is present in the encoders
+    if degree == 'Bachelors':
+        degree_encoded = 0
+    elif degree == 'Masters':
+        degree_encoded = 1
+    else:
+        degree_encoded = 2
+    # degree_encoded = degree_encode.transform([degree])[0]  
+    job_title_encoded = job_title_encoder.transform([str(job_title)])[0]
+    print(job_title_encoded)
+    country_encoded  = country_encoder.transform([country])[0]
+    age_scaled = scaler_age.transform(np.array([[age]]))
+    experience_scaled = scaler_experience.transform(np.array([[experience_years]]))
+    
+    # Combine inputs into a single array
+    input_data = np.array([[age_scaled[0][0], experience_scaled[0][0], degree_encoded, job_title_encoded, country_encoded]])
+    
+    # Predict the salary
+    predicted_salary = round(model.predict(input_data)[0],4)
+    print(predicted_salary)
+    
+    return jsonify({'predicted_salary': predicted_salary})
+if __name__ == "__main__":
     app.run(debug=True)
+
